201682c082789ce28e1fac92e1d0b9fd788e3749
LeHuyHung_20194579 <hung.lh194579@sis.hust.edu.vn>
2023-06-07 22:43:39 +0700

    new list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import { useState, useEffect } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 import ListTeacher from './components/ListTeacher';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header1 from './components/Header1';
 import Footer1 from './components/Footer1';
-import teacherTest from './data/teacherTest';
+// import teacherTest from './data/teacherTest';
 
 //import teacherTest from "./data/teacherTest";
 
 function App() {
   const [teachers, setTeachers] = useState([]);
 
-  // useEffect(() => {
-  //   async function fetchTeacher() {
-  //     try {
-  //       const response = await axios.get(
-  //         "http://127.0.0.1:8000/api/teacher"
-  //       );
-  //       setTeachers(response.data.data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   fetchTeacher();
-  // }, []);
+  useEffect(() => {
+    async function fetchTeacher() {
+      try {
+        const response = await axios.get(
+          "https://be-marathonwebsite-ruler-production-93fe.up.railway.app/api/teacher?fbclid=IwAR1HucjzXBSZ8B1BIFnzWSgmYxOMlsPdvNOxK9r7ikWJa1GlVWYaLGOCe9Y"
+        );
+        setTeachers(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchTeacher();
+  }, []);
   // useEffect(() => {
   //   // Kiểm tra xem dữ liệu đã được lưu trong Local Storage chưa
   //   const existingData = localStorage.getItem('myData');
@@ -34,15 +34,15 @@ function App() {
   //   }
   // }, []);
 
-  useEffect(() => {
-    // Lấy dữ liệu từ LocalStorage
-    const storedData = localStorage.getItem('myData');
+  // useEffect(() => {
+  //   // Lấy dữ liệu từ LocalStorage
+  //   const storedData = localStorage.getItem('myData');
 
-    if (storedData) {
-      // Nếu dữ liệu đã tồn tại trong LocalStorage, gán vào state
-      setTeachers(JSON.parse(storedData));
-    }
-  }, []);
+  //   if (storedData) {
+  //     // Nếu dữ liệu đã tồn tại trong LocalStorage, gán vào state
+  //     setTeachers(JSON.parse(storedData));
+  //   }
+  // }, []);
   return (
     <div className="App">
       <BrowserRouter>
