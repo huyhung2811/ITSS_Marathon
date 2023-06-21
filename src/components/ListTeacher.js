736b311dad1fa00fd93ee25c5ebe2b03893aa126
thanhtuan0310 <thanhtuan102001@gmail.com>
2023-06-21 19:02:10 +0700

    Teacher List

--- src/components/ListTeacher.js
+++ src/components/ListTeacher.js
@@ -1,64 +1,71 @@
 import React, { useState } from "react";
 import { Pagination } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ShowTeacher from "./ShowTeacher";
 import FilterComponent from './FilterComponent';
 
 import "./ListTeacher.css"
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 
-function ListTeacher({teachers}) {
-  const [currentPage, setCurrentPage] = useState(1);
+function ListTeacher({ teachers }) {
+    const [currentPage, setCurrentPage] = useState(1);
 
 
-  const pageSize = 6;
+    const pageSize = 6;
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-
-  const currentTeacher = teachers.slice(
-    (currentPage - 1) * pageSize,
-    currentPage * pageSize
-  );
-
-  const totalItems = teachers.length;
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
 
-  const handleFilterSubmit = filterData => {
-    // Xử lý logic khi nhận được các giá trị filter
-    // Gọi API hoặc thực hiện các tác vụ liên quan
-    console.log(filterData); // Ví dụ: In ra dữ liệu filter trong console
+    const currentTeacher = teachers.slice(
+        (currentPage - 1) * pageSize,
+        currentPage * pageSize
+    );
+    const totalItems = teachers.length;
 
-    // Sau khi xử lý xong, có thể cập nhật dữ liệu kết quả vào state "results"
-  };
 
-  return (
-    <div className="listTeacher-container">
-       <div className="filter-container">
-        <FilterComponent onSubmit={handleFilterSubmit} />
-      </div>   
-      <div className="list-paginate">
-      <div className = "filter-results"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <ShowTeacher currentTeacher={ currentTeacher } />
-      </div>
-      <div className = "paginate-numbers">
-      <Pagination
-          current={currentPage}
-          pageSize={pageSize}
-          total={totalItems}
-          onChange={handlePageChange}
-        />
+    const handleFilterSubmit = filterData => {
+        // Xử lý logic khi nhận được các giá trị filter
+        // Gọi API hoặc thực hiện các tác vụ liên quan
+        console.log(filterData); // Ví dụ: In ra dữ liệu filter trong console
+
+        // Sau khi xử lý xong, có thể cập nhật dữ liệu kết quả vào state "results"
+    };
+
+    return (
+        <div>
+            <div className="m-0 py-1 my-1" style={{ backgroundColor: '#ADD8E1' }}>
+                <p className="fw-bold fs-4 m-0"><FontAwesomeIcon className="mx-5" icon={faFilter} style={{ color: '#1A9360'}} />条件に合わせて<span className="text-danger">最適な教師</span>を見つける！</p>
+            </div>
+            <div className="listTeacher-container">
+                <div className="filter-container">
+                    <FilterComponent onSubmit={handleFilterSubmit} />
+                </div>
+
+                <div className="list-paginate">
+                    <div className="filter-results"
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <ShowTeacher currentTeacher={currentTeacher} />
+                    </div>
+                    <div className="paginate-numbers">
+                        <Pagination
+                            current={currentPage}
+                            pageSize={pageSize}
+                            total={totalItems}
+                            onChange={handlePageChange}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  );
+    );
 }
 
 export default ListTeacher;
