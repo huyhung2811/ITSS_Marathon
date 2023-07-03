90fd2aa54149d97deaf8cbf37c062163cbee09cc
LeHuyHung_20194579 <hung.lh194579@sis.hust.edu.vn>
2023-07-03 21:28:27 +0700

    new-fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,14 @@ import Profile from "../components/Profile";
 import TeacherDetails from "../components/TeacherDetails";
 import DefaultLayout from "../layout/defaultLayout/DefaultLayout"
 import Home from "../pages/home";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
 
 export const publicRoutes = [
     { path: '/list-teacher', element: ListTeacher, layout: DefaultLayout },
     { path: '/teacher/:id', element: TeacherDetails, layout: DefaultLayout },
     { path: '/profile', element: Profile, layout: DefaultLayout },
     { path: '/', element: Home, layout: DefaultLayout },
+    { path: '/signin', element: SignIn, layout: DefaultLayout},
+    { path: '/signup', element: SignUp, layout: DefaultLayout},
 ];
