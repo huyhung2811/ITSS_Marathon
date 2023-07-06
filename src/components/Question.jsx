1c7579545e38a96e042a5074b50483aa7363b606
dazai-osamu-111 <nguyentheduc9alacve@gmail.com>
2023-07-06 15:02:58 +0700

    ducnt

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -31,6 +31,7 @@ const YourComponent = () => {
                 // Handle error here
                 console.error(error);
             });
+        window.location.href = '/list-teacher';
     };
 
     return (
