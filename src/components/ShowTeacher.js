d2e431e11ec944f1a5fb0867bda73a852f514e04
tonghuutham <tonghuutham@gmail.com>
2023-07-11 22:44:51 +0700

    fix

--- src/components/ShowTeacher.js
+++ src/components/ShowTeacher.js
@@ -1,71 +1,71 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
+import './ListTeacher.css';
+import './ShowTeacher.css';
 
-import "./ListTeacher.css"
-import "./ShowTeacher.css"
-
-import Rating from "@mui/material/Rating";
-import axios from "axios";
+import Rating from '@mui/material/Rating';
+import axios from 'axios';
 
 // currentTeacher là danh sách các giáo viên cần in
 function ShowTeacher({ currentTeacher }) {
-  const a = "%";
-  const toggleHeart = (teacherId) => {
-    axios
-      .post('https://be-marathonwebsite-ruler-production-6ad6.up.railway.app/api/bookmark', {
-        teacher_id: teacherId,
-        user_id: 1
-      })
-      .then((response) => {
-        console.log('thanh cong');
-      })
-      .catch((error) => {
-        console.log('loi');
-      });
-  };
-  return (
-    <Row xs={3} md={3} className="g-4" style={{ marginTop: "5px" }}>
-      {currentTeacher.map((teacher, idx) => (
+    const a = '%';
+    const toggleHeart = (teacherId) => {
+        axios
+            .post('https://be-marathonwebsite-ruler-production-6ad6.up.railway.app/api/bookmark', {
+                teacher_id: teacherId,
+                user_id: 1,
+            })
+            .then((response) => {
+                console.log('thanh cong');
+            })
+            .catch((error) => {
+                console.log('loi');
+            });
+    };
+    return (
+        <Row xs={3} md={3} className="g-4" style={{ marginTop: '5px' }}>
+            {currentTeacher.map((teacher, idx) => (
+                <Col key={idx}>
+                    <Card style={{ border: '1px solid #000' }}>
+                        <div className="card">
+                            <div className="avatar-background"></div>
+                            <Link to={`/teacher/${teacher.id}`} className="link-no-underline">
+                                <div className="avatar-wrapper">
+                                    <Card.Img
+                                        variant="top"
+                                        src={teacher.avatar}
+                                        style={{
+                                            width: '90%',
+                                            height: '230px',
+                                            borderRadius: '50%',
+                                            objectFit: 'cover',
+                                            marginLeft: '5%',
+                                        }}
+                                    />
+                                </div>
+                            </Link>
 
-        <Col key={idx}>
-          <Card style={{ border: "1px solid #000" }}>
-            <div className="card">
-              <div className="avatar-background"></div>
-              <Link to={`/teacher/${teacher.id}`} className="link-no-underline">
-                <div className="avatar-wrapper">
-                  <Card.Img
-                    variant="top"
-                    src={teacher.avatar}
-                    style={{
-                      width: '90%',
-                      height: '230px',
-                      borderRadius: '50%',
-                      objectFit: 'cover',
-                      marginLeft: '5%'
-                    }}
-                  />
-                </div>
-              </Link>
-
-              <Card.Body
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Card.Title>{teacher.name}{teacher.age}</Card.Title>
-                <Card.Text>
-                  {teacher.point && Math.round(teacher.point * 100) + a}
-                  {/* <div style={{ alignItems: "center" }}>
+                            <Card.Body
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Card.Title>
+                                    {teacher.name} , {teacher.age}
+                                </Card.Title>
+                                <Card.Text>
+                                    {teacher.point && Math.round(teacher.point * 100) + a}
+                                    {/* <div style={{ alignItems: "center" }}>
                     {teacher.point&&teacher.point >= 0.8 ? (
                       <div style={{ color: "green" }}>Math.round(teacher.point * 100) + a</div>
                     ) : teacher.point >= 0.5 && teacher.point<=0.8 ? (
@@ -74,35 +74,41 @@ function ShowTeacher({ currentTeacher }) {
                       <div style={{ color: "red" }}>{Math.round(teacher.point * 100) + a}</div>
                     )}
                   </div> */}
-                  <br />
-                  <strong>レベル:</strong> {teacher.level}
-                  <br />
-                  {teacher.address + ", Ha Noi"}
-                  <br />
-                  <span>
-                    <Rating name="half-rating-read" defaultValue={parseFloat(teacher.vote)} precision={0.1} readOnly style={{ backgroundColor: "#fff" }} /></span>
-                  {teacher.bookmark ? (
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      style={{ marginLeft: "50px", color: "red" }}
-                      onClick={() => toggleHeart(teacher.id)}
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      style={{ marginLeft: "50px" }}
-                      onClick={() => toggleHeart(teacher.id)}
-                    />
-                  )}
-                </Card.Text>
-              </Card.Body>
-            </div>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-
-  );
+                                    <br />
+                                    <strong>レベル:</strong> {teacher.level}
+                                    <br />
+                                    {teacher.address + ', Ha Noi'}
+                                    <br />
+                                    <span>
+                                        <Rating
+                                            name="half-rating-read"
+                                            defaultValue={parseFloat(teacher.vote)}
+                                            precision={0.1}
+                                            readOnly
+                                            style={{ backgroundColor: '#fff' }}
+                                        />
+                                    </span>
+                                    {teacher.bookmark ? (
+                                        <FontAwesomeIcon
+                                            icon={faHeart}
+                                            style={{ marginLeft: '50px', color: 'red' }}
+                                            onClick={() => toggleHeart(teacher.id)}
+                                        />
+                                    ) : (
+                                        <FontAwesomeIcon
+                                            icon={faHeart}
+                                            style={{ marginLeft: '50px' }}
+                                            onClick={() => toggleHeart(teacher.id)}
+                                        />
+                                    )}
+                                </Card.Text>
+                            </Card.Body>
+                        </div>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    );
 }
 
 export default ShowTeacher;
